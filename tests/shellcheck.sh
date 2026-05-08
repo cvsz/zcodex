69b08d8dafb5d88a19aac3dfdbe70612827eb4bd
cvsz <seaza@msn.com>
2026-05-09 00:56:40 +0700

    Format shellcheck test with shfmt

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-mapfile -t shell_files < <({ printf '%s\n' codex.sh; find scripts tests -type f -name '*.sh'; } | sort)
+mapfile -t shell_files < <({
+	printf '%s\n' codex.sh
+	find scripts tests -type f -name '*.sh'
+} | sort)
 shellcheck "${shell_files[@]}"
